48cbf1ad698b4a6f04fb7143d5de709d0401e31b
foodDinner <specialdelivery@gmail.com>
2019-12-16 21:03:29 -0600

    more stuff from exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloControllerDynamic.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloControllerDynamic.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HelloControllerDynamic {
 
@@ -24,5 +27,27 @@ public class HelloControllerDynamic {
         String greeting = "Hello " + name + "!";
         model.addAttribute("greeting", greeting);
         return "hello";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names",names);
+        return "hello-list";
+    }
+    // need to change form.html method and action to GET and hello-localized, respectively, for this to work. old stuff
+    @GetMapping("hello-localized")
+    @ResponseBody
+    public String hello(@RequestParam String name, String lang) {
+        if (lang.equals("English")) {
+            return "Hello, " + name + "!";
+        }
+        else if (lang.equals("French")) {
+            return "Bonjour, " + name + "!";
+        }
+        else return name + " " + lang;
     }
 }
